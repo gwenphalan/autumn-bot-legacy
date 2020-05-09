9429c91542b9b4c123ccf0ef52d983fc935b9d7c
Gwen <creepthewolf@icloud.com>
2020-05-08 20:43:58 -0400

    port

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ const { Guild, router } = require(__dirname + '/guild.js');
 const bodyParser = require('body-parser');
 const verification = require(__dirname + "/modules/verification.js");
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env['PORT'] || 3000;
 
 app.use(bodyParser.urlencoded({
   extended: false
