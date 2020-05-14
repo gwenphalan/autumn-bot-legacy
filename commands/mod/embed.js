16d448f5b318f5b82e29f392fe932ba590412092
Gwen <creepthewolf@icloud.com>
2020-05-14 05:36:30 -0400

    News

--- commands/mod/embed.js
+++ commands/mod/embed.js
@@ -1,94 +1,103 @@
-const commando = require('discord.js-commando');
-const Discord = require('discord.js');
-const oneLine = require('common-tags').oneLine;
+const commando = require("discord.js-commando");
+const Discord = require("discord.js");
+const oneLine = require("common-tags").oneLine;
 
 module.exports = class ClassName extends commando.Command {
-    constructor(client) {
-        super(client, {
-            name: 'embed',
-            aliases: [],
-            group: 'mod',
-            memberName: 'embed',
-            description: 'Sends an embeded message to the chosen channel.',
-            details: oneLine`
+  constructor(client) {
+    super(client, {
+      name: "embed",
+      aliases: [],
+      group: "mod",
+      memberName: "embed",
+      description: "Sends an embeded message to the chosen channel.",
+      details: oneLine`
                 description
             `,
-            examples: ["-embed #channel 'Title' 'This is the embeds description.' '#24fc03'"],
+      examples: [
+        "-embed #channel 'Title' 'This is the embeds description.' '#24fc03'",
+      ],
 
-            args: [
-                {
-                    key: 'channel',
-                    prompt: 'What channel would you like to send the embedded message to?',
-                    type: 'channel'
-                },
-                {
-                    key: 'title',
-                    prompt: 'What would you like the title to be?',
-                    type: 'string'
-                },
-                {
-                    key: 'desc',
-                    prompt: 'What would you like the description to say?',
-                    type: 'string'
-                },
-                {
-                    key: 'color',
-                    prompt: 'What color would you like the embed to be?',
-                    type: 'string'
-                },
-                {
-                    key: 'messageId',
-                    prompt: 'If you are editting an embed, what is its message ID?',
-                    type: 'string',
-                    default: 'new'
-                }
-              ]
+      args: [
+        {
+          key: "channel",
+          prompt:
+            "What channel would you like to send the embedded message to?",
+          type: "channel",
+        },
+        {
+          key: "title",
+          prompt: "What would you like the title to be?",
+          type: "string",
+        },
+        {
+          key: "desc",
+          prompt: "What would you like the description to say?",
+          type: "string",
+        },
+        {
+          key: "color",
+          prompt: "What color would you like the embed to be?",
+          type: "string",
+        },
+        {
+          key: "messageId",
+          prompt: "If you are editting an embed, what is its message ID?",
+          type: "string",
+          default: "new",
+        },
+      ],
+    });
+  }
+
+  async run(msg, { channel, title, desc, color, messageId }) {
+    if (
+      !msg.member.hasPermission("ADMINISTRATOR") &&
+      msg.author.id != "279910519467671554"
+    ) {
+      msg
+        .reply("You don't have the `ADMINISTRATOR` permission!")
+        .then(function (msg) {
+          msg.delete(10000);
+        });
+      msg.delete();
+      return;
+    }
+
+    const embed = new Discord.MessageEmbed()
+      .setColor(color)
+      .setTitle(title)
+      .setAuthor(msg.author.username, msg.author.displayAvatarURL())
+      .setDescription(desc);
+
+    const created = new Discord.MessageEmbed()
+      .setColor("#db583e")
+      .setAuthor(
+        "Custom Embed",
+        "https://cdn.discordapp.com/avatars/672548437346222110/3dcd9d64a081c6781289b3e3ffda5aa2.png?size=128"
+      )
+      .setTitle("Embed Created!");
+
+    const editted = new Discord.MessageEmbed()
+      .setColor("#db583e")
+      .setAuthor(
+        "Custom Embed",
+        "https://cdn.discordapp.com/avatars/672548437346222110/3dcd9d64a081c6781289b3e3ffda5aa2.png?size=128"
+      )
+      .setTitle("Embed Editted!");
+
+    if (messageId === "new") {
+      channel.send(embed);
+
+      msg.channel.send(created);
+    } else {
+      channel.messages
+        .fetch({ around: messageId, limit: 1 })
+        .then((msg) => {
+          const fetchedMsg = msg.first();
+          fetchedMsg.edit(embed);
         })
+        .catch(console.error);
+      msg.channel.send(editted);
     }
-
-    async run(msg, {channel, title, desc, color, messageId}) {
-        if (!msg.member.hasPermission('ADMINISTRATOR') && msg.author.id != "279910519467671554"){
-            msg.reply("You don't have the `ADMINISTRATOR` permission!").then(function (msg) { 
-                msg.delete(10000)
-            });
-            msg.delete();
-            return;
-        };
-        
-        const embed = new Discord.MessageEmbed()
-        .setColor(color)
-        .setTitle(title)
-        .setAuthor(msg.author.username, msg.author.displayAvatarURL())
-        .setDescription(desc);
-        
-        const created = new Discord.MessageEmbed()
-        .setColor('#db583e')
-        .setAuthor('Custom Embed', 'https://cdn.discordapp.com/avatars/672548437346222110/3dcd9d64a081c6781289b3e3ffda5aa2.png?size=128')
-        .setTitle('Embed Created!');
-        
-        const editted = new Discord.MessageEmbed()
-        .setColor('#db583e')
-        .setAuthor('Custom Embed', 'https://cdn.discordapp.com/avatars/672548437346222110/3dcd9d64a081c6781289b3e3ffda5aa2.png?size=128')
-        .setTitle('Embed Editted!');
-
-
-        if(messageId === "new"){
-
-        channel.send(embed);
-        
-        msg.channel.send(created);
-
-        }else{
-        
-            channel.messages.fetch({around: messageId, limit: 1})
-                .then(msg => {
-                const fetchedMsg = msg.first();
-                fetchedMsg.edit(embed);
-            })
-            .catch(console.errror);
-            msg.channel.send(editted);
-
-        }
-        
-    }
-}
+  }
+};
--- commands/util/news.js
+++ commands/util/news.js
@@ -1,30 +1,35 @@
-const commando = require('discord.js-commando');
-const oneLine = require('common-tags').oneLine;
-const Discord = require('discord.js')
+const commando = require("discord.js-commando");
+const oneLine = require("common-tags").oneLine;
+const Discord = require("discord.js");
 
 module.exports = class ClassName extends commando.Command {
-    constructor(client) {
-        super(client, {
-            name: 'news',
-            aliases: [],
-            group: 'util',
-            memberName: 'news',
-            description: 'Displays recent bot news',
-            details: oneLine`
+  constructor(client) {
+    super(client, {
+      name: "news",
+      aliases: [],
+      group: "util",
+      memberName: "news",
+      description: "Displays recent bot news",
+      details: oneLine`
                 Displays recent bot news.
             `,
-            examples: ['-help'],
-        })
-    }
+      examples: ["-help"],
+    });
+  }
 
-    async run(msg) {
-        const help = new Discord.MessageEmbed()
-            .setAuthor('News', 'https://cdn.discordapp.com/avatars/672548437346222110/3dcd9d64a081c6781289b3e3ffda5aa2.png?size=256')
-            .setDescription('The dashboard has been released! Now you can manage each of your servers\' settings from one place, see which servers your bot is in, and disable/enable different modules. At the moment, the Verification Module is the only one, but more are coming in the future.\n\n**0.0.4 Changelog | Dashboard Update**\n\n- `-setup` and `-settings` no longer exist! You can now setup your server and change your settings straight from the [dashboard](https://www.autumnbot.net/dashboard)!\n- Bot now removes non-verified to verify users instead of adding member. This was causing conflicts with channel permissions when updating bot permissions.\n- Updated bot from discordjs-v11 to discordjs-v12\n\n**To get regular updates on the bot\'s development, join the official [discord server](https://discord.gg/DfByvyN)!**')
-            .setColor('#db583e')
-            .setURL('https://discord.gg/DfByvyN')
-            .setFooter(`Requested By ${msg.author.tag}`)
+  async run(msg) {
+    const help = new Discord.MessageEmbed()
+      .setAuthor(
+        "News",
+        "https://cdn.discordapp.com/avatars/672548437346222110/3dcd9d64a081c6781289b3e3ffda5aa2.png?size=256"
+      )
+      .setDescription(
+        "Recently the bot has seen some major downtime, this is due to rate limiting from the discord API. I am currently rewriting the entire bot. Hopefully the more efficient code will bring a better experience with Autumn Bot. The new bot will be entirely command based, with the dashboard as an option. The new dashboard, however, will not be released for sometime. If you have any questions, feel free to join the support server.\n\n**To get regular updates on the bot's development, join the official [discord server](https://discord.gg/DfByvyN)!**"
+      )
+      .setColor("#db583e")
+      .setURL("https://discord.gg/DfByvyN")
+      .setFooter(`Requested By ${msg.author.tag}`);
 
-        msg.channel.send(help);
-    }
-}
+    msg.channel.send(help);
+  }
+};
